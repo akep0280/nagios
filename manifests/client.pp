1ce2083b9b5e7ea8906613e1162f8a0a9547f3cd
akep0280 <akepley@gmail.com>
2015-07-30 13:46:42 -0500

    Nagios mod

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,8 +1,15 @@
 class nagios::client {
-
-   file { "/root/linux-nrpe-agent":
+#provide the installation directory and content to the client
+  file { "/root/linux-nrpe-agent":
     source => "puppet:///modules/nagios/linux-nrpe-agent",
     recurse => true,
     }
 
+#if xinetd.conf does not exist, run the install script
+  exec { 'fullinstall':
+    command => "fullinstall",
+    cwd     => "/root/linux-nrpe-agent",
+    creates => "/etc/xinetd.conf",
+    path    => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
+    }
 }
